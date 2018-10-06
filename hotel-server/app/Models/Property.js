916a67d1bd3427a43844cc2cdd6653f233c904bd
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-10-06 14:17:33 -0300

    funcionando update post get

--- hotel-server/app/Models/Property.js
+++ hotel-server/app/Models/Property.js
@@ -1,8 +1,23 @@
 'use strict'
 
 const Model = use('Model')
+const Database = use('Database')
 
 class Property extends Model {
+
+  static scopeNearBy(query, latitude, longitude, distance) {
+    const haversine = `(6371 * acos(cos(radians(${latitude}))
+      * cos(radians(latitude))
+      * cos(radians(longitude)
+      - radians(${longitude}))
+      + sin(radians(${latitude}))
+      * sin(radians(latitude))))`
+
+    return query
+      .select('*', Database.raw(`${haversine} as distance`))
+      .whereRaw(`${haversine} < ${distance}`)
+  }
+
   user() {
     return this.belongsTo('App/Models/User')
   }
@@ -11,9 +26,6 @@ class Property extends Model {
     return this.hasMany('App/Models/Image')
   }
 
-  static scopeNearBy (query, latidude, longitude, distance) {
-    return query
-  }
 }
 
 module.exports = Property
