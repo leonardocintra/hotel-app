cd803c5646f33037288c67d275017343f2201068
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-10-06 15:44:55 -0300

    parando para ir depois

---
+++ hotel-web/src/pages/singIn/index.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { StatusBar, AsyncStorage } from 'react-native';
+import { StackActions, NavigationActions } from 'react-navigation';
+
+import { Container, Logo, Input, ErrorMessage, Button, ButtonText } from './styles';
+
+export default class SingIn extends Component {
+  render() {
+    return (
+      <Container>
+        <StatusBar hidden />
+        <Logo source={require('../../images/airbnb_logo.png')} resizeMode="contain" />
+        <Input
+          placeholder="Endereço de email"
+          value={this.state.email}
+          onChangeText={this.handleEmailChange}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Input
+          placeholder="Senha"
+          value={this.state.password}
+          onChangeText={this.handlePasswordChange}
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry
+        />
+        {this.state.error.length !== 0 && <ErrorMessage>{this.state.error}</ErrorMessage>}
+        <Button onPress={this.handleSingInPress}>
+          <ButtonText>Entrar</ButtonText>
+        </Button>
+        <SignUpLink onPress={this.handleCreateAccountPress}>
+          <SignUpLinkText>Criar conta grátis</SignUpLinkText>
+        </SignUpLink>
+      </Container>
+    )
+  }
+
+  static navigationOptions = {
+    header: null,
+  };
+
+  state = { email: '', password: '', error: '' };
+
+  handleEmailChange = (email) => {
+    this.setState({ email });
+  };
+
+  handlePasswordChange = (password) => {
+    this.setState({ password });
+  };
+
+  handleCreateAccountPress = () => {
+    this.props.navigation.navigate('SignUp');
+  };
+
+  handleSignInPress = async () => {
+    if (this.state.email.length === 0 || this.state.password.length === 0) {
+      this.setState({ error: 'Preencha usuário e senha para continuar!' }, () => false);
+    } else {
+      try {
+        const response = await api.post('/sessions', {
+          email: this.state.email,
+          password: this.state.password,
+        });
+
+        await AsyncStorage.setItem('@AirBnbApp:token', response.data.token);
+
+        const resetAction = StackActions.reset({
+          index: 0,
+          actions: [
+            NavigationActions.navigate({ routeName: 'Main' }),
+          ],
+        });
+        this.props.navigation.dispatch(resetAction);
+      } catch (_err) {
+        this.setState({ error: 'Houve um problema com o login, verifique suas credenciais!' });
+      }
+    }
+
+    // Parei em https://blog.rocketseat.com.br/react-native-autenticacao/
+    /*
+    Nessa linha
+    
+    Agora falta criarmos a função handleSignInPress que é a responsável pela comunicação com a API e dar um resultado de sucesso ou erro para o Login, irei primeiro postar o código e logo abaixo explico o fluxo dela.
+
+    */
+  }
